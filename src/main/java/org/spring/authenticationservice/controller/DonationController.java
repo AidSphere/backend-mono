4e971aed9a07abe10efcaf04a12c204ce54f4789
kaweesha-mr <kaweesha.mr@gmail.com>
2025-03-13 14:30:25 +0530

    ref: donor functions added

---
+++ src/main/java/org/spring/authenticationservice/controller/DonationController.java
@@ -0,0 +1,11 @@
+package org.spring.authenticationservice.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/donation-request")
+public class DonationController {
+
+
+}
+
