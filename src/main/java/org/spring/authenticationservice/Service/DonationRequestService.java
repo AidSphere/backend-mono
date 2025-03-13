b30804e5b546e6f094a6af5df16d137057e9cc9f
kaweesha-mr <kaweesha.mr@gmail.com>
2025-03-13 20:42:45 +0530

    chore:service files updated -validation are not setup yet

--- src/main/java/org/spring/authenticationservice/Service/DonationRequestService.java
+++ src/main/java/org/spring/authenticationservice/Service/DonationRequestService.java
@@ -1,25 +1,26 @@
 package org.spring.authenticationservice.Service;
 
 
-import org.spring.authenticationservice.model.DonationRequest;
+import org.spring.authenticationservice.model.DonationPatientRequests;
 import org.spring.authenticationservice.model.Enum.StatusEnum;
-import org.spring.authenticationservice.repository.security.DonationRequestRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.spring.authenticationservice.repository.DonationRequestRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DonationRequestService {
+    private final DonationRequestRepository donationRequestRepository;
 
-    @Autowired
-    private DonationRequestRepository donationRequestRepository;
+    public DonationRequestService(DonationRequestRepository donationRequestRepository) {
+        this.donationRequestRepository = donationRequestRepository;
+    }
 
-    public List<DonationRequest> getPendingDonationRequests() {
-        //return donationRequestRepository.findByStatus(StatusEnum.PENDING);
-        List<DonationRequest> donationRequests = donationRequestRepository.findAll();
-        System.out.println(donationRequests);
-        return donationRequests;
+    public List<DonationPatientRequests> getPendingDonationRequests() {
+        return donationRequestRepository.findByStatus(StatusEnum.PENDING);
+    }
+
+    public DonationPatientRequests createDonationRequest(DonationPatientRequests donationPatientRequests) {
+        return donationRequestRepository.save(donationPatientRequests);
     }
 }
