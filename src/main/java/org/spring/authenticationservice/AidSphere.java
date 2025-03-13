06b3cf4f380543c3cd13928b38dcadf19b6e1b3f
kaweesha-mr <kaweesha.mr@gmail.com>
2025-03-13 14:11:57 +0530

    ref: auth service renamed to AidSphere

--- src/main/java/org/spring/authenticationservice/AidSphere.java
+++ src/main/java/org/spring/authenticationservice/AidSphere.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationServiceApplication {
+public class AidSphere {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthenticationServiceApplication.class, args);
+        SpringApplication.run(AidSphere.class, args);
     }
 
 }
