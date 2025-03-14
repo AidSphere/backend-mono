b730278260def3a8f680b00cce04b47c09d44b41
kaweesha-mr <kaweesha.mr@gmail.com>
2025-03-15 05:10:31 +0530

    chore:donation request function added

--- src/main/java/org/spring/authenticationservice/controller/DonationRequestController.java
+++ src/main/java/org/spring/authenticationservice/controller/DonationRequestController.java
@@ -1,6 +1,10 @@
 package org.spring.authenticationservice.controller;
+import org.spring.authenticationservice.DTO.DonationRequestConfirmationDTO;
 import org.spring.authenticationservice.Service.DonationRequestService;
 import org.spring.authenticationservice.model.DonationPatientRequests;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,15 +19,30 @@ public class DonationRequestController {
         this.donationRequestService = donationRequestService;
     }
 
+    @PostMapping("/create")
+    public DonationPatientRequests createDonation(@RequestBody DonationPatientRequests donationPatientRequests) {
+        return donationRequestService.createDonationRequest(donationPatientRequests);
+    }
+
     @GetMapping("/pending")
     public List<DonationPatientRequests> getPendingDonations() {
         return donationRequestService.getPendingDonationRequests();
     }
 
-    @PostMapping("/create")
-    public DonationPatientRequests createDonation(@RequestBody DonationPatientRequests donationPatientRequests) {
-        return donationRequestService.createDonationRequest(donationPatientRequests);
+    @PutMapping("/confirmation")
+    public ResponseEntity<?> confirmDonationRequest(
+            @RequestBody DonationRequestConfirmationDTO donationRequestConfirmationDTO,
+            @RequestParam Long requestId) {
+
+        try {
+            return donationRequestService.updateConfirmation(requestId, donationRequestConfirmationDTO);
+        } catch (ChangeSetPersister.NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Donation request not found");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the request");
+        }
     }
+
 
 
 }
--- src/main/java/org/spring/authenticationservice/model/DonationPatientRequests.java
+++ src/main/java/org/spring/authenticationservice/model/DonationPatientRequests.java
@@ -6,10 +6,10 @@ import lombok.Data;
 import org.spring.authenticationservice.model.Enum.StatusEnum;
 import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Data
 public class DonationPatientRequests {
 
     @Id
@@ -43,5 +43,77 @@ public class DonationPatientRequests {
     private Long adminId;
 
     @JsonProperty("adminApprovedDate")
-    private Date adminApprovedDate;
+    private LocalDateTime adminApprovedDate;
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getMessageToPatient() {
+        return messageToPatient;
+    }
+
+    public void setMessageToPatient(String messageToPatient) {
+        this.messageToPatient = messageToPatient;
+    }
+
+    public String getPrescriptionLink() {
+        return prescriptionLink;
+    }
+
+    public void setPrescriptionLink(String prescriptionLink) {
+        this.prescriptionLink = prescriptionLink;
+    }
+
+    public Long getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
+    }
+
+    public LocalDateTime getAdminApprovedDate() {
+        return adminApprovedDate;
+    }
+
+    public void setAdminApprovedDate(LocalDateTime adminApprovedDate) {
+        this.adminApprovedDate = adminApprovedDate;
+    }
 }
