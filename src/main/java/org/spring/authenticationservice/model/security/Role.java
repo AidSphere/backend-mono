1bde334608ad3000fde6083a621918a2ee590918
kaweesha-mr <kaweesha.mr@gmail.com>
2025-03-23 11:51:50 +0530

    feat: switch case condition

--- src/main/java/org/spring/authenticationservice/model/security/Role.java
+++ src/main/java/org/spring/authenticationservice/model/security/Role.java
@@ -9,7 +9,7 @@ public class Role {
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String name; // Example: "ADMIN", "USER"
+    private String name;
 
     public Long getId() {
         return id;
