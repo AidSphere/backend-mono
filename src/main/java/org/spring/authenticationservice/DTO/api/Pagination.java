c254bbcf2ad42a56b5461c1890b3397b24ba0b29
ushan senarathna <ushansenarathna000@gmail.com>
2025-03-28 03:31:00 +0530

    chore: setup api response with pagination

---
+++ src/main/java/org/spring/authenticationservice/DTO/api/Pagination.java
@@ -0,0 +1,19 @@
+package org.spring.authenticationservice.DTO.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pagination {
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean isLast;
+    private boolean isFirst;
+}
