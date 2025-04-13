83daf316d7fbc6cd7488a97bf3b7e2bdfc59c72f
kaweesha-mr <kaweesha.mr@gmail.com>
2025-04-13 11:20:54 +0530

    feat(admin): add AccountController for managing patients, donors, and drug importers

---
+++ src/main/java/org/spring/authenticationservice/controller/admin/AccountController.java
@@ -0,0 +1,99 @@
+package org.spring.authenticationservice.controller.admin;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.spring.authenticationservice.DTO.api.ApiResponse;
+import org.spring.authenticationservice.DTO.donor.DonorRegDto;
+import org.spring.authenticationservice.DTO.drugImporter.DrugImporterRegisterRequest;
+import org.spring.authenticationservice.DTO.patient.PatientCreateDto;
+import org.spring.authenticationservice.Service.donor.DonorService;
+import org.spring.authenticationservice.Service.drugImporter.DrugImporterService;
+import org.spring.authenticationservice.Service.patient.PatientService;
+import org.spring.authenticationservice.Service.security.AuthService;
+import org.spring.authenticationservice.controller.drugImporter.DrugImporterController;
+import org.spring.authenticationservice.mapper.patient.PatientMapper;
+import org.spring.authenticationservice.model.donor.Donor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.spring.authenticationservice.controller.drugImporter.DrugImporterController.getCurrentRequestPath;
+
+@RestController
+@RequestMapping("/admin")
+@AllArgsConstructor
+public class AccountController {
+
+    private PatientService patientService;
+    private PatientMapper patientMapper;
+    private DrugImporterService drugImporterService;
+    private DonorService donorService;
+    private AuthService   authService;
+
+
+    //create patient
+    @PostMapping("/createPatient")
+    public ResponseEntity<ApiResponse<?>> createPatient(@Valid @RequestBody PatientCreateDto dto) {
+        var createdPatient = patientService.createPatient(dto);
+        var responseDto = patientMapper.toResponseDto(createdPatient);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.CREATED.value())
+                        .message("Patient Created Successfully")
+                        .data(responseDto)
+                        .build());
+    }
+
+    //create donor
+    @PostMapping("/createDonor")
+    public ResponseEntity<ApiResponse<?>> createDonor(@Valid @RequestBody DonorRegDto dto) {
+        Donor createdDonor = donorService.createDonor(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.CREATED.value())
+                        .message("Donor Created Successfully")
+                        .data(createdDonor)
+                        .build());
+    }
+
+
+    //create drug importer
+    @PostMapping(value = "/createDrgImporter", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<Void>> registerDrugImporter(
+            @RequestBody @Valid DrugImporterRegisterRequest request) throws Exception {
+
+        drugImporterService.registerDrugImporter(request);
+
+        ApiResponse<Void> response = ApiResponse.<Void>builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.CREATED.value())
+                .message("Drug Importer registered successfully.")
+                .path(getCurrentRequestPath())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<ApiResponse<?>> getAllUsers() {
+
+        //get all users from authservices as a list and return
+        List<?> users = authService.getAllUsers();
+
+
+        return ResponseEntity.ok(ApiResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.OK.value())
+                .message("Fetched all users successfully")
+                .data(users) // Replace with actual user data
+                .build());
+    }
+}
