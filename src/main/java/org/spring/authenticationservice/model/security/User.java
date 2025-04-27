81ca7db82138f42d0b18ccbaa5f1974dff2c8ac6
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2025-04-27 20:52:25 +0530

    feat(user): set default value of isEnabled to true in User entity

--- src/main/java/org/spring/authenticationservice/model/security/User.java
+++ src/main/java/org/spring/authenticationservice/model/security/User.java
@@ -21,7 +21,7 @@ public class User {
     private String password;
 
     @Column(nullable = false)
-    private boolean isEnabled;
+    private boolean isEnabled = true;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
