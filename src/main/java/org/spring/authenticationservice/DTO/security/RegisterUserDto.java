f868c61e13317f313f5bded29322c4f08f135cc9
kaweesha-mr <kaweesha.mr@gmail.com>
2025-05-02 20:05:30 +0530

    feat(security): add CORS configuration and update DTOs with Lombok annotations

--- src/main/java/org/spring/authenticationservice/DTO/security/RegisterUserDto.java
+++ src/main/java/org/spring/authenticationservice/DTO/security/RegisterUserDto.java
@@ -1,10 +1,14 @@
 package org.spring.authenticationservice.DTO.security;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterUserDto {
     @JsonProperty("email")
     private String email;
